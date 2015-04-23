5dd24790d7dab83499b72e5b51860577502fccab
fengping13579 <767319995@qq.com>
2015-04-23 16:47:15 +0800

    tankuang

--- js/table.js
+++ js/table.js
@@ -1,38 +1,21 @@
- $(function(){
- 	$("tr:even").css("background-color","#eee");
- 	$(document).on("click","tr td",function(){
- 	// 单个点击反复事件	
- 	//	$(this).toggleClass("b-d")
- 	
- 	//  点击时当前出现某样式 ，
- 		$("tr td").removeClass("b-d");
- 		$(this).addClass("b-d")
- 	})
- })
-// function Dialog(config){
-//      var defaults = {
-//          name:'',                  //窗口的类型
-//          width:'200',              //宽度
-//          height:'100',             //高度
-//          top:'50%',                //距离高度的位置
-//          left:'50%',               //距离右边的高度
-//          backgroundColor:'#000',   //背景颜色
-//          opacity:'0.8',            //背景颜色透明度
-//          icon:'',                  //提示图标的地址
-//          text:'',                  //提示的文字
-//          'zIndex':1000             //背景的层级
-//      };
-//      
-//      //接收外面传来的参数
-//      this.config = $.extend({},defaults,config);
-//      
-//      this.bgID = '';
-//  }
+   $(function(){
+   	$("tr:even").css("background-color","#eee");
+   	$(document).on("click","tr td",function(){
+   	// 单个点击反复事件	
+   	//	$(this).toggleClass("b-d")
+   	
+   	//  点击时当前出现某样式 ，
+   		$("tr td").removeClass("b-d");
+   		$(this).addClass("b-d")
+   	})
+   })
+
 function showInfoTips(text) {
-				var dialog=({
+				$.dialog({
 					content: "<p type='text-align:center;'>"+ text + "</p>", // 内容
 					width: 400, // 宽度
 					height:100,
+					title:"<h3>温馨提示</h3>",
 					cancel:function(){
 						return true;
 					},
@@ -42,28 +25,6 @@ function showInfoTips(text) {
 				});
 			}
 
-
-//function showInfoTips(text) {
-//				 $.dialog({
-//					content: "<p type='text-align:center;'>"+ text + "</p>", // 内容
-//					width: 400, // 宽度
-//					height:100,
-//					cancel:function(){
-//						return true;
-//					},
-//					cancelText:'知道了',
-//					lock: true, // 是否锁屏
-//					zIndex: 9999 // z-index值
-//				});
-//			}
-function show1(){
-			$.messager.show({
-				title:'My Title',
-				msg:'Message will be closed after 4 seconds.',
-				showType:'show'
-			});
-		}
-
 $(function(){
 	$(document).on("click",".submit",function(){
 		var username=$("#username").val();
@@ -71,24 +32,24 @@ $(function(){
 		var password1=$("#password1").val();
 		if(username.length==0){
 			
-			alert("请输入用户名")
-			return;
+			showInfoTips("请输入昵称");
+            return;
 			
 		}else if(username.length>=7){
-			alert("输入的用户名过长");
+			showInfoTips("输入的用户名过长");
 			return;
 		}
 		if(password.length==0){
-			alert("请输入密码");
+			showInfoTips("请输入");
 			
 		}else if(password.length<=4){
-			alert("输入的密码过短");
+			showInfoTips("输入的密码过短");
 			
 		}
-		if(password===password1){
-			alert("密码一致");
+		if(password1.length==0){
+			showInfoTips("请再次输入密码");
 		}else {
-			alert("密码不一致")
+			showInfoTips("密码不一致")
 		}
 		
 	})			
@@ -104,17 +65,7 @@ $(function(){
 
 
 
-//$(function(){
-//	$(document).on("click",".submit",function(){
-//		var username=$("#username").val();
-//		if(username.length==""){
-//			alert("请输入用户名");
-//			
-//		}else if(username.length>=7){
-//			alert("输入的用户名过长")			
-//		}								
-//	})			
-//})
+
 
 
 
